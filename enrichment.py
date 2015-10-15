a1b21dc231264839672091ec782a027b40c4fc40
Derek Britain <dbritain34@gmail.com>
2015-10-14 17:19:05 -0700

    implemented enrich function and list_genes lambda

--- enrichment.py
+++ enrichment.py
@@ -2,15 +2,26 @@
 from intensities import *
 
 
+def enrich(df, col, upper, lower):
+    upreg = df[
+        (df[col] > upper) &
+        np.isfinite(df[col])].index
+    downreg = df[
+        (df[col] < lower) &
+        np.isfinite(df[col])].index
+    return upreg, downreg
+
 # target = wcl_foldch[
 # ((wcl_foldch["Intensity Shmoo_CaCl2_WCL"] > 3) |
 # (wcl_foldch["Intensity Shmoo_CaCl2_WCL"] < -3)) &
 # np.isfinite(wcl_foldch["Intensity Shmoo_CaCl2_WCL"])].index
-target = wcl_foldch[
-    (wcl_foldch["Intensity Shmoo_CaCl2_WCL"] > 3) &
-    np.isfinite(wcl_foldch["Intensity Shmoo_CaCl2_WCL"])].index
+# target = wcl_foldch[
+#     (wcl_foldch["Intensity Shmoo_CaCl2_WCL"] > 3) &
+#     np.isfinite(wcl_foldch["Intensity Shmoo_CaCl2_WCL"])].index
 
-target = [t.replace(';', '\n') for t in target]
+wcl_cacl2_up, wcl_cacl2_down = enrich(wcl_foldch, "Intensity Shmoo_CaCl2_WCL", 3, -3)
+
+list_genes = lambda x: [t.replace(';', '\n') for t in x]
 
 with open('data/enrichment/WCL_CaCl2_up_gt3.txt', 'w') as f:
-    f.write('\n'.join(target))
+    f.write('\n'.join(list_genes(wcl_cacl2_up)))
