98b5a560b5dbdbc4f32fb7624114aefea745b9a6
Daniel Asarnow <daniel.asarnow@ucsf.edu>
2015-10-15 21:19:07 -0700

    Write up and downregulated lists.

--- enrichment.py
+++ enrichment.py
@@ -30,9 +30,13 @@ def write_enrichment(enr, fname):
 def enrichment(df, cols, upper, lower):
     for c in df[cols].columns:
         up, down = enrich(df, c, upper, lower)
-        write_enrichment(up, os.path.join(enrichdir, c + '.txt'))
+        write_enrichment(up, os.path.join(enrichdir, c + '_up' + '.txt'))
+        write_enrichment(down, os.path.join(enrichdir, c + '_down' + '.txt'))
+
 
 enrichment(wcl_foldch, wcl_exp, 3, -3)
 enrichment(wclp_foldch, wclp_exp, 3, -3)
 enrichment(ub_foldch, ub_exp, 3, -3)
 enrichment(ubp_foldch, ubp_exp, 3, -3)
+
+gsy = pd.read_table('data/go_slim_mapping.tab', low_memory=False)
