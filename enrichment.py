36133189c8f72996c160573b65086b6e00db2deb
Daniel Asarnow <daniel.asarnow@ucsf.edu>
2015-10-15 22:54:48 -0700

    Made enrichment use a main method, so it can be imported without trouble.

--- enrichment.py
+++ enrichment.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python2.7
 from intensities import *
+import os
 import os.path
 
 
-def enrich(df, col, upper, lower):
-    upreg = df[
-        (df[col] >= upper) &
-        np.isfinite(df[col])].index
-    downreg = df[
-        (df[col] <= lower) &
-        np.isfinite(df[col])].index
+def threshold_columns(df, col, upper, lower):
+    upreg = (df[col] >= upper) & np.isfinite(df[col])
+    downreg = (df[col] <= lower) & np.isfinite(df[col])
     return upreg, downreg
 
 
-enrichdir = 'data/enrichment'
-
-
 def list_genes(enr):
-    genes = [t.replace(';', '\n') for t in enr]
+    genes = [t.replace(';', os.linesep) for t in enr]
     genes = [g for g in genes if not (("CON" in g) | ("REV" in g))]
     return genes
 
 
 def write_enrichment(enr, fname):
     with open(fname, 'w') as f:
-        f.write('\n'.join(list_genes(enr)) + '\n')
+        f.write(os.linesep.join(list_genes(enr)) + os.linesep)
 
 
 def enrichment(df, cols, upper, lower):
+    enrichdir = 'data/enrichment'
     for c in df[cols].columns:
-        up, down = enrich(df, c, upper, lower)
-        write_enrichment(up, os.path.join(enrichdir, c + '_up' + '.txt'))
-        write_enrichment(down, os.path.join(enrichdir, c + '_down' + '.txt'))
+        up, down = threshold_columns(df, c, upper, lower)
+        write_enrichment(df[up].index, os.path.join(enrichdir, c + '_up' + '.txt'))
+        write_enrichment(df[down].index, os.path.join(enrichdir, c + '_down' + '.txt'))
 
 
-enrichment(wcl_foldch, wcl_exp, 3, -3)
-enrichment(wclp_foldch, wclp_exp, 3, -3)
-enrichment(ub_foldch, ub_exp, 3, -3)
-enrichment(ubp_foldch, ubp_exp, 3, -3)
+def main():
+    enrichment(wcl_foldch, wcl_exp, 3, -3)
+    enrichment(wclp_foldch, wclp_exp, 3, -3)
+    enrichment(ub_foldch, ub_exp, 3, -3)
+    enrichment(ubp_foldch, ubp_exp, 3, -3)
 
-gsy = pd.read_table('data/go_slim_mapping.tab', low_memory=False)
+    gsy = pd.read_table('data/go_slim_mapping.tab', header=None, low_memory=False).set_index(0)
+    # wcl_go = wcl_foldch.reset_index().merge(
+    #         gsy,
+    #         how="inner",
+    #         left_on="Protein IDs",
+    #         right_on=0
+    #     ).set_index("Protein IDs")
+
+
+if __name__ == "__main__":
+    main()
